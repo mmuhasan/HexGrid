fa31a5e7f404b0b041dc50d84577487ed1acf35e
mhrasel <mhrasel@gmail.com>
2016-07-26 22:20:35 -0230

    test code

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 package hexGrid2;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Test {
 
@@ -12,25 +14,57 @@ public class Test {
 			data[i] = new myClass(i);
 
 		HexGrid<myClass> objHexGrid = new HexGrid<myClass>(9,11,data);
-		int slice = 6;
+		int slice = -1;
 		ArrayList<myClass> list = objHexGrid.slice(slice);
-		for(int i=0;i<list.size();i++)
-			list.get(i).print();
-		System.out.println("finish");
-	
-		HexGrid<myClass> objHexGrid2 = new HexGrid<myClass>(9,6,data);
+		Collections.sort(list);
 		
-		ArrayList<myClass> list2 = objHexGrid2.slice(slice);
-		for(int i=0;i<list2.size();i++)
-			list2.get(i).print();
+		myClass A;
+		for(int i=0;i<list.size();i++)
+		{
+			A = list.get(i);
+			if(i+1 != A.a)
+			{ 	
+				System.out.printf("%d. ",i+1);
+				A.print();
+			}
+		}
 		System.out.println("finish");
-	}
-	
 	
 
+		HexGrid<myClass> objHexGrid2 = new HexGrid<myClass>(9,6,data);
+		ArrayList<myClass> list2 = objHexGrid2.slice(slice);
+		Collections.sort(list2);
+		
+		for(int i=0;i<list2.size();i++)
+		{
+			A = list2.get(i);
+			if(i+1 != A.a)
+			{ 	
+				System.out.printf("%d. ",i+1);
+				A.print();
+			}
+		}
+		System.out.println("finish");
+
+		HexGrid<myClass> objHexGrid3 = new HexGrid<myClass>(10,10,data);
+		ArrayList<myClass> list3 = objHexGrid2.slice(slice);
+		Collections.sort(list3);
+		
+		for(int i=0;i<list3.size();i++)
+		{
+			A = list3.get(i);
+			if(i+1 != A.a)
+			{ 	
+				System.out.printf("%d. ",i+1);
+				A.print();
+			}
+		}
+		System.out.println("finish");
+	
+	}
 }
 
-class myClass
+class myClass implements Comparable<myClass>
 {
 	int a;
 	myClass(int i)
@@ -45,5 +79,10 @@ class myClass
 	void print()
 	{
 		System.out.println(a+" : "+sqre());
+	}
+
+	@Override
+	public int compareTo(myClass o) {
+		return this.a - o.a;
 	}
 }
